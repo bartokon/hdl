9ae269c55ac66d6e1ccf7735af02a90e8bac5904
Bartek <bartekrycko@gmail.com>
2024-05-15 11:23:18 +0200

    Fix uvm_skid_buffer tb, drivers, sprintf statements, add reset_sequence for driver

--- uvm_skid_buffer/uvm_driver.sv
+++ uvm_skid_buffer/uvm_driver.sv
@@ -10,7 +10,8 @@ class axi4_stream_driver #(string interface_name = "") extends uvm_driver #(sequ
 
     virtual axi4_stream vif;
     sequence_item item;
-
+    integer count;
+    
     `uvm_component_utils(axi4_stream_driver#(.interface_name(interface_name)))
     function new(input string name = "axi4_stream_driver", uvm_component parent = null);
         super.new(name, parent);
@@ -22,21 +23,19 @@ class axi4_stream_driver #(string interface_name = "") extends uvm_driver #(sequ
                 `uvm_fatal("DRV", "Could not get axi4_stream vif");
             end
             item = sequence_item::type_id::create("sequence_item");
+            count = 0;
     endfunction
 
     virtual task run_phase(uvm_phase phase);
         super.run_phase(phase);
             forever begin
-                `uvm_info("DRV", $sformatf("Wait for item from sequencer"), UVM_LOW);
                 seq_item_port.get_next_item(item);
-                 `uvm_info("DRV", $sformatf("Got item from sequencer"), UVM_LOW);
-                drive_item(item);
-                 `uvm_info("DRV", $sformatf("Wait for next item from sequencer"), UVM_LOW);
+                drive_item(item, count++);
                 seq_item_port.item_done();
             end
     endtask
 
-    virtual task drive_item(sequence_item item);
+    virtual task drive_item(sequence_item item, integer count);
     endtask
     
 endclass
@@ -52,19 +51,34 @@ class axi4_stream_master_driver #(string interface_name = "") extends axi4_strea
         super.build_phase(phase);
     endfunction
     
-    virtual task drive_item(sequence_item item);
-        vif.data <= item.data;
-        vif.rstn <= item.resetn;
-        vif.valid <= 1;
-        @(posedge vif.clk)
-        wait (vif.ready && vif.valid);
-//            while (!vif.ready) begin
-//                `uvm_info("axi4_stream_master driver", "Wait until ready is high", UVM_LOW);
-//                @(posedge vif.clk);
-//            end
-        @(negedge vif.clk);
-        vif.valid <= 0;
+    virtual task reset_phase(uvm_phase phase);
+        super.reset_phase(phase);
+        phase.raise_objection(this);
+        `uvm_info("", "Reset From driver...!", UVM_LOW)
         vif.data <= 0;
+        vif.rstn <= 0;
+        vif.valid <= 0;
+        #100 vif.rstn <= 1;
+        phase.drop_objection(this);
+    endtask
+    
+    virtual task drive_item(sequence_item item, integer count);
+        `uvm_info("MASTER DRIVER", $sformatf("Started driving item. %d", count), UVM_LOW);
+        @(posedge vif.clk) begin 
+            vif.data <= item.data;
+            vif.rstn <= item.resetn;
+            vif.valid <= 1;
+        end
+        forever begin 
+            @(posedge vif.clk) begin 
+                if (vif.ready && vif.valid) break;
+            end 
+        end
+        @(negedge vif.clk) begin 
+            vif.valid <= 0;
+            vif.data <= 'x;
+        end
+        `uvm_info("MASTER DRIVER", $sformatf("Done driving item. %d", count), UVM_LOW);
     endtask
 
 endclass
@@ -81,15 +95,20 @@ class axi4_stream_slave_driver #(string interface_name = "") extends axi4_stream
         vif.ready <= 0;
     endfunction
     
-    virtual task drive_item(sequence_item item);
-        vif.ready <= 1;
-        @(posedge vif.clk);
-        wait (vif.ready && vif.valid);
-//        while (!vif.valid) begin
-//            `uvm_info("axi4_stream_slave_driver", "Wait until valid is high", UVM_LOW);
-//        end     
-        @(negedge vif.clk);
-        vif.ready <= 0;
+    virtual task drive_item(sequence_item item, integer count);
+        `uvm_info("SLAVE DRIVER", $sformatf("Started driving item. %d", count), UVM_LOW);
+        @(posedge vif.clk) begin 
+            vif.ready <= 1;
+        end
+        forever begin 
+            @(posedge vif.clk) begin 
+                if (vif.ready && vif.valid) break;
+            end 
+        end
+        @(negedge vif.clk) begin
+            vif.ready <= 0;
+        end
+        `uvm_info("SLAVE DRIVER", $sformatf("Done driving item. %d", count), UVM_LOW);
     endtask
 
 endclass
